008cd3684d45aeb8181ebdd628abb4d82b0b04f6
HemishPansuriya <hemishnandlal@gmail.com>
2026-03-10 12:18:41 +0530

    Enhance comments in InventoryConfig class
    Added comments to clarify the purpose of the InventoryConfig class and its methods.

--- inventory/apps.py
+++ inventory/apps.py
@@ -2,8 +2,18 @@ from django.apps import AppConfig
 
 
 class InventoryConfig(AppConfig):
+    # This sets the default primary key field type for models in this app
+    # BigAutoField means Django will use a large auto-incrementing integer as the primary key
     default_auto_field = 'django.db.models.BigAutoField'
+
+    # This is the name of the Django app
     name = 'inventory'
 
     def ready(self):
+        # The ready() method runs when the Django app is fully loaded
+        # It is commonly used to import signals so that they get registered
+        # when Django starts the application.
+
+        # This line imports the signals.py file from the inventory app
+        # Without this import, the signals inside signals.py will not run
         import inventory.signals
